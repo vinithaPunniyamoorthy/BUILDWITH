dc3769ab9e1ee4b82a607c95f9892f1af3ea7a0f
vinithaPunniyamoorthy <162816259+vinithaPunniyamoorthy@users.noreply.github.com>
2025-04-08 21:46:22 +0530

    https://www.figma.com/design/k6BaoP677rH6bgsq61gKBU/Build-with?node-id=0-1&p=f&t=2aGEmTpJb85AV4iZ-0

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import { useState } from 'react'
-import './App.css'
-import Organizations from './components/Organizations'
-import Navbar from './components/Navbar/Navbar'
-import Home from './components/House/home'
-import Details from './components/Details/Details'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Organizations from "./components/Landpage";
+import Navbar from "./components/Navbar/Navbar";
+import Details from "./components/Details/Details";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home/>
-      <Details />
-      <Organizations />
-      
-    </>
-  )
+    <Router>
+      <div className="App">
+        <Navbar />
+        <nav className="main-nav">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About Us</Link></li>
+            <li><Link to="/details">Details</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Organizations />} />
+          <Route path="/about" element={<Organizations />} />
+          <Route path="/details" element={<Details />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Landpage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Landpage.css';
+
+const Organizations = () => {
+    return (
+        <div className="organizations-container">
+            <div className="row">
+                <div className="who-we-are-section">
+                    <div className="who-we-are-content">
+                        <h2>Who We Are</h2>
+                        <p className="who-we-are-text">
+                            <strong>BUILDWITH</strong> is a non-profit organization dedicated to empowering underprivileged communities through education, skill development, and sustainable initiatives. We believe in creating opportunities for growth and development, focusing on areas that need it the most.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Organizations;
